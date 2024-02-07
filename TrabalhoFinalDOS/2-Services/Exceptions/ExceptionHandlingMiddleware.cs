9b475da08d1162e502c0c7b2deb756d0840cc60b
Asaph Alves <1221473@isep.ipp.pt>
2024-02-07 19:29:01 +0000

    Lidar com execeções.

--- TrabalhoFinalDOS/2-Services/Exceptions/ExceptionHandlingMiddleware.cs
+++ TrabalhoFinalDOS/2-Services/Exceptions/ExceptionHandlingMiddleware.cs
@@ -1,4 +1,6 @@
-﻿namespace TrabalhoFinalDOS._2_Services.Exceptions
+﻿using System.Net;
+
+namespace TrabalhoFinalDOS._2_Services.Exceptions
 {
     public class ExceptionHandlingMiddleware
     {
@@ -9,6 +11,39 @@
         {
             _next = next;
             _logger = logger;
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            _logger.LogError(exception, "Ocorreu um erro.");
+            ExceptionResponse response = exception switch
+            {
+                ExcepcaoHTTP _ => new ExceptionResponse(((ExcepcaoHTTP)exception).HTTPCode, ((ExcepcaoHTTP)exception).Mensagem),
+                _ => new ExceptionResponse(HttpStatusCode.InternalServerError, "Ocorreu um erro interno")
+            };
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)response.StatusCode;
+            var responseObject = new
+            {
+                mensagem = response.Mensagem
+            };
+            await context.Response.WriteAsJsonAsync(responseObject);
+        }
+        public record ExceptionResponse(HttpStatusCode StatusCode, string Mensagem);
+
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
         }
 
 
