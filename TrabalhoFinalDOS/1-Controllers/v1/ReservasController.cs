c0c0a58ebc88a1d799ea2136529ecde501a75ffc
Tiago Marquez <1221508@isep.ipp.pt>
2024-02-07 20:46:15 +0000

    Servico de Reservas DTO E NovaReservaDTO (Criar Reserva)

---
+++ TrabalhoFinalDOS/1-Controllers/v1/ReservasController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrabalhoFinalDOS.DTO;
+using TrabalhoFinalDOS.Models;
+using TrabalhoFinalDOS.Services;
+
+namespace TrabalhoFinalDOS.Controllers
+{ 
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class ReservasController : ControllerBase
+    {
+        private readonly ILogger<ReservasController> _logger;
+        private readonly IReservasService _servicoReservas;
+
+        public ReservasController(ILogger<ReservasController> logger, IReservasService servicoReservas)
+        {
+            _logger = logger;
+            _servicoReservas = servicoReservas;
+        }
+
+
+     
+
+        [HttpPost("")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(NovaReservaDTO))]
+        public IActionResult CriarReserva([FromBody] NovaReservaDTO body)
+        {
+            var reservaCriada = _servicoReservas.CriarReserva(body);
+            return Ok(reservaCriada);
+        }
+    }
+}
