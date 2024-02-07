8696e5d1f003c5fcfd790104c89c6fed6f4e5ab6
Asaph Alves <1221473@isep.ipp.pt>
2024-02-07 19:27:31 +0000

    Inicio das classes para lidar com execções

---
+++ TrabalhoFinalDOS/2-Services/Exceptions/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,16 @@
+﻿namespace TrabalhoFinalDOS._2_Services.Exceptions
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+
+    }
+}
