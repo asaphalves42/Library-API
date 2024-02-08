44eee41e8c9108c9c13630f0392126305e012d44
David Fernandes <1221777@isep.ipp.pt>
2024-02-08 21:44:06 +0000

    Configuração da base de dados, InicialMigration e Alteração da Password da base de dados

---
+++ TrabalhoFinalDOS/Migrations/20240208212710_InitialMigration.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrabalhoFinalDOS.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "clientes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    telemovel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clientes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "livros",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    titulo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    autor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ano = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_livros", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reservas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    id_cliente = table.Column<int>(type: "int", nullable: false),
+                    id_livro = table.Column<int>(type: "int", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reservas", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_reservas_clientes_id_cliente",
+                        column: x => x.id_cliente,
+                        principalTable: "clientes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_reservas_livros_id_livro",
+                        column: x => x.id_livro,
+                        principalTable: "livros",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reservas_id_cliente",
+                table: "reservas",
+                column: "id_cliente");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reservas_id_livro",
+                table: "reservas",
+                column: "id_livro");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "reservas");
+
+            migrationBuilder.DropTable(
+                name: "clientes");
+
+            migrationBuilder.DropTable(
+                name: "livros");
+        }
+    }
+}
